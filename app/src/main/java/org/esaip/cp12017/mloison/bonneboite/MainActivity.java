3ef2f448e5976e14bc9e7808d0ce0f20c4aa34d8
mloison <mloison.cpi2019@esaip.org>
2018-01-18 18:06:21 +0100

    Requete asynchrone authentification API ok

--- app/src/main/java/org/esaip/cp12017/mloison/bonneboite/MainActivity.java
+++ app/src/main/java/org/esaip/cp12017/mloison/bonneboite/MainActivity.java
@@ -2,12 +2,31 @@ package org.esaip.cp12017.mloison.bonneboite;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    private APIRequestAsyncConnectionActivity Auth;
+        /** Called when the activity is first created. */
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            Auth = new APIRequestAsyncConnectionActivity();
+            Auth.execute();
+            String test = Auth.getServer_response();
+        } // end onCreate()
+
+    protected void onResultButtonClick(View v){
+        TextView result = (TextView)findViewById(R.id.textViewResult);
+        if (Auth.getServer_response_code() == 200) {
+            result.setText(Auth.getServer_response());
+        }
     }
-}
+    }
