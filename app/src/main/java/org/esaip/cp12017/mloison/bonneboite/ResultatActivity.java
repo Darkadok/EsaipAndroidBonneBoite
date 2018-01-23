aa428b1465831382d29390a5bdb60a90578c9a57
mloison <mloison.cpi2019@esaip.org>
2018-01-23 11:28:00 +0100

    Resultat activity added

---
+++ app/src/main/java/org/esaip/cp12017/mloison/bonneboite/ResultatActivity.java
@@ -0,0 +1,16 @@
+package org.esaip.cp12017.mloison.bonneboite;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Darkadok on 23/01/2018.
+ */
+
+public class ResultatActivity extends AppCompatActivity {
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultat);
+    }
+}
