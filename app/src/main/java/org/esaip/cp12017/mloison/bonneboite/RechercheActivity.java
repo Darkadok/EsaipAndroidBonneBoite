1c0f0adea5aa2c1dfa605467dc6f647bddfbf693
mloison <mloison.cpi2019@esaip.org>
2018-01-23 11:04:01 +0100

    Request search

--- app/src/main/java/org/esaip/cp12017/mloison/bonneboite/RechercheActivity.java
+++ app/src/main/java/org/esaip/cp12017/mloison/bonneboite/RechercheActivity.java
@@ -4,11 +4,18 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import java.util.HashMap;
+
 /**
  * Created by Darkadok on 23/01/2018.
  */
 
 public class RechercheActivity extends AppCompatActivity {
+    private String token;
+
+    public RechercheActivity(String token) {
+        this.token = token;
+    }
 
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -16,6 +23,14 @@ public class RechercheActivity extends AppCompatActivity {
     }
 
     public void onClick_LancerRecherche(View v){
-
+        APIRequest request = new APIRequest(token, construireParametres());
+        request.execute();
     }
+
+    private HashMap<String, String> construireParametres(){
+        HashMap<String, String> toReturn = new HashMap<>();
+        toReturn.put("commune_id", "00000");
+        return toReturn;
+    }
+    //From zipcode to INSEE code by choosing a city
 }
