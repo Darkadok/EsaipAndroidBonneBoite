bd2a916745b175279ec2b7609f3a74c169602d16
mloison <mloison.cpi2019@esaip.org>
2018-01-23 10:50:46 +0100

    Begining of search interface

--- app/src/main/java/org/esaip/cp12017/mloison/bonneboite/RechercheActivity.java
+++ app/src/main/java/org/esaip/cp12017/mloison/bonneboite/RechercheActivity.java
@@ -2,6 +2,7 @@ package org.esaip.cp12017.mloison.bonneboite;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by Darkadok on 23/01/2018.
@@ -12,5 +13,9 @@ public class RechercheActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recherche);
+    }
+
+    public void onClick_LancerRecherche(View v){
+
     }
 }
