53815b33a83c53ff54c9d6e347ec66e7db98971c
mloison <mloison.cpi2019@esaip.org>
2018-01-24 08:55:51 +0100

    companieRecyclerViewAdapter added

--- app/src/main/java/org/esaip/cp12017/mloison/bonneboite/ResultatActivity.java
+++ app/src/main/java/org/esaip/cp12017/mloison/bonneboite/ResultatActivity.java
@@ -2,12 +2,16 @@ package org.esaip.cp12017.mloison.bonneboite;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 
 /**
  * Created by Darkadok on 23/01/2018.
  */
 
 public class ResultatActivity extends AppCompatActivity {
+
+    companieAdapter _adapter;
+    RecyclerView _recyclerView;
 
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
---
+++ app/src/main/java/org/esaip/cp12017/mloison/bonneboite/companieAdapter.java
@@ -0,0 +1,54 @@
+package org.esaip.cp12017.mloison.bonneboite;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Darkadok on 24/01/2018.
+ */
+
+public class companieAdapter extends RecyclerView.Adapter<companieAdapter.CompanieViewHolder> {
+    private List<companie> _companies;
+
+
+    public void onArticlesReceived(List<companie> companies, boolean hasMore) {
+        _companies.clear();
+        _companies.addAll(companies);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        return _companies.size();
+    }
+
+    @Override
+    public CompanieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_companie, parent, false);
+        return new CompanieViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(CompanieViewHolder holder, int position) {
+        holder.bind(_companies.get(position));
+    }
+
+    public static class CompanieViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView _title;
+
+        public CompanieViewHolder(View view) {
+            super(view);
+            _title = (TextView) view.findViewById(R.id.title);
+        }
+
+        public void bind(companie c) {
+            _title.setText(c.getName());
+        }
+    }
+}
